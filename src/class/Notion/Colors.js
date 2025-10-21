cc952ec563aa542782fe0c59daba0af6cfcb64fd
AdamPastel <=>
2025-10-21 19:48:41 +0200

    Added Notion Colors and Languages support

---
+++ src/class/Notion/Colors.js
@@ -0,0 +1,24 @@
+
+const Colors = Object.freeze({
+    Blue: "blue",
+    Blue_Background: "blue_background",
+    Brown: "brown",
+    Brown_Background: "brown_background",
+    Default: "default",
+    Gray: "gray",
+    Gray_Background: "gray_background",
+    Green: "green",
+    Green_Background: "green_background",
+    Orange: "orange",
+    Orange_Background: "orange_background",
+    Pink: "pink",
+    Pink_Background: "pink_background",
+    Purple: "purple",
+    Purple_Background: "purple_background",
+    Red: "red",
+    Red_Background: "red_background",
+    Yellow: "yellow",
+    Yellow_Background: "yellow_background"
+})
+
+export default Colors;
