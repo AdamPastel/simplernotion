a78b48384d934e8b2cfb0445d0a6025c16c9b9eb
AdamPastel <=>
2025-10-21 19:51:08 +0200

    Added a Page Builder to create pages

---
+++ src/class/PageBuilder.js
@@ -0,0 +1,99 @@
+"use strict";
+import Utils from '../utils.js';
+import Block from './Blocks/Block.js';
+
+export default class PageBuilder {
+    constructor () {
+        this.icon = null;
+        this.cover = null;
+        this.title = null;
+        this.properties = {};
+        this.content = [];
+    }
+
+    /**
+     * @param {string} icon - The icon of the page
+     * @example
+     * // Set the icon of the page to the speficied image url
+     *  new PageBuilder()
+     *      .setIcon("👋")
+     */
+    setIcon(icon) {
+        this.icon = icon;
+        return this;
+    }
+    
+    /**
+     * @param {string} cover_url - The cover of the page
+     * @example
+     * // Set the Cover of the page to the speficied image URL
+     *  new PageBuilder()
+     *      .setCover("https://website.com/a-beautiful-cover.png")
+     */
+    setCover(cover_url) {
+        this.cover = cover_url;
+        return this;
+    }
+    
+    /**
+     * @param {string} title - The title of the page
+     * @example
+     * // Set the Title of the page to the speficied value
+     *  new PageBuilder()
+     *      .setTitle("A brand new page!")
+     */
+    setTitle(title) {
+        this.title = title; 
+        return this;
+    }
+
+    /**
+     * @param {Object} properties - An object containing the properties and their values
+     * @example
+     * // Set the Description and Number properties to the specified values
+     *  new PageBuilder()
+     *      .setProperties({
+     *          Description: "The answer to the universe",
+     *          Number: 42
+     *      })
+     */
+    setProperties(properties) {
+        this.properties = properties;
+        return this;
+    }
+
+    /**
+     * @param {Array<Block>} content - A list of various blocks
+     * @example
+     * // Add a paragraph to the page content
+     *  new PageBuilder()
+     *      .setContent([
+     *          new Paragraph()
+     *              .setText("Hello world!")
+     *      ])
+     */
+    setContent(content) {
+        this.content = content;
+        return this;
+    }
+
+    /**
+     * @private
+     */
+    _toNotion(source) {
+        let titleKey;
+        for (var key in source.properties) {
+            if (source.properties.hasOwnProperty(key) && source.properties[key].type === "title") titleKey = key;
+        }
+        return {
+            parent: {
+                type: 'data_source_id',
+                data_source_id: source?.id
+            },
+            properties: Object.assign({ [titleKey]: { title: [{ type: "text", text: { content: this.title } }] } }, Utils.convertProperties(source.properties, this.properties)),
+            cover: this.cover ? { type: "external", external: { url: this.cover } } : undefined,
+            icon: this.icon ? { type: "emoji", emoji: this.icon } : undefined,
+            children: this.content.flatMap(c => c._toNotion()),
+        };
+    }
+}
